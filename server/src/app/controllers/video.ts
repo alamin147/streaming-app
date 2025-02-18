4cb7ca0a253d1cdf7b918b84e66828885446308c
alamin147 <alamin.14780@gmail.com>
2025-02-18 17:53:14 +0600

    update and delete feature

--- server/src/app/controllers/video.ts
+++ server/src/app/controllers/video.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import Video from "../models/Video";
 import { response } from "../utils/utils";
+import { createError } from "../error/error";
 
 export const createVideo = async (
   req: Request,
@@ -28,5 +29,50 @@ export const getVideo = async (
     response(res, 200, true, "Video fetched successfully", { video });
   } catch (err) {
     next(err);
+  }
+};
+
+export const updateVideo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return response(res, 404, false, "Video not found!");
+
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      response(res, 200, true, "Video updated successfully", { updatedVideo });
+    } else {
+      return response(res, 403, false, "You can update only your video!");
+    }
+  } catch (err: any) {
+    response(res, 500, false, err.message || "Internal Server Error");
+  }
+};
+
+export const deleteVideo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return response(res, 404, false, "Video not found!");
+    if (req.user.id === video.userId) {
+      await Video.findByIdAndDelete(req.params.id);
+      res.status(200).json("The video has been deleted.");
+    } else {
+      return response(res, 403, false, "You can delete only your video!");
+    }
+  } catch (err: any) {
+    response(res, 500, false, err.message || "Internal Server Error");
   }
 };
--- server/src/app/routes/video.ts
+++ server/src/app/routes/video.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { verifyToken } from "../utils/utils";
-import { createVideo, getVideo } from "../controllers/video";
+import {
+  createVideo,
+  deleteVideo,
+  getVideo,
+  updateVideo,
+} from "../controllers/video";
 
 const videoRoutes = express.Router();
 
 videoRoutes.post("/", verifyToken, createVideo);
 videoRoutes.get("/find/:id", getVideo);
-
+videoRoutes.put("/:id", verifyToken, updateVideo);
+videoRoutes.delete("/:id", verifyToken, deleteVideo);
 export default videoRoutes;
