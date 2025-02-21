119f32ad25923b51204b09d924b51c71b484922a
alamin147 <alamin.14780@gmail.com>
2025-02-21 23:36:13 +0600

    header styling

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,49 +1,52 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { Button } from "../ui/button";
 import { useTheme } from "../themeProvider/ThemeProvider";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 export default function Sidebar() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
+        <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4">
+          <div className="flex items-center gap-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <a href="#" className="text-sm font-medium">
+              All
+            </a>
+            <a href="#" className="text-sm font-medium">
+              Movies
+            </a>
+            <a href="#" className="text-sm font-medium">
+              Trending
+            </a>
           </div>
-          <ul>
-            <Button onClick={() => setTheme("dark")}>Dark</Button>
-            <Button onClick={() => setTheme("light")}>Light</Button>
-          </ul>
+          <div className="flex items-center gap-4">
+            {theme == "dark" ? (
+              <Sun
+                onClick={() => setTheme("light")}
+                className="cursor-pointer"
+              />
+            ) : (
+              <Moon
+                onClick={() => setTheme("dark")}
+                className="cursor-pointer"
+              />
+            )}
+            <Button className="dark:bg-yellow-500" size="sm">
+              Login
+            </Button>
+          </div>
         </header>
         {/*  main data */}
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
